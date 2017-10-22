f0515fd1cd0508fa5d01ea08882911cc1ba22ff3
Ethan Brooks <EB5473@gmail.com>
2017-10-22 00:56:23 -0400

    indents will be the death of me

--- src/main/java/com/elytradev/phosphorus/Phosphorus.java
+++ src/main/java/com/elytradev/phosphorus/Phosphorus.java
@@ -2,8 +2,8 @@
  *  The MIT License (MIT)
  *
  *  Copyright (c) 2017:
- *	  Ethan Brooks (CalmBit),
- *	  and contributors
+ *      Ethan Brooks (CalmBit),
+ *      and contributors
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of
  *  this software and associated documentation files (the "Software"), to deal in
